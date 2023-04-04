d9f93d97c0d4e075b779e2d531f389dab6ee0532
Octane-XT <zorakotoasimbola@hotmail.com>
2023-04-05 01:44:27 +0600

    exercice

--- SI/application/controllers/Exercice.php
+++ SI/application/controllers/Exercice.php
@@ -1,44 +1,36 @@
 <?php
-defined('BASEPATH') or exit('No direct script access allowed');
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Exercice extends CI_Controller
-{
-    public function __construct()
-    {
-        parent::__construct();
-        $this->load->model('Exercice_model');
-    }
-    public function index()
-    {
-        $this->load->view('header');
-        $data['comptes'] = $this->Exercice_model->get_exercice();
-        $this->load->view('exercice/add');
-        //$this->load->view('exercice/list', $data);
-        $this->load->view('footer');
-    }
+class Exercice extends CI_Controller {
+  public function __construct() {
+    parent::__construct();
+    $this->load->model('Exercice_model');
+  }
+  public function index() {
+    //$data['exercice'] = $this->Exercice_model->getDateFin();
+    $this->load->view('exercice/index');
+  }
 
-    public function add()
-    {
-        if ($this->input->post()) {
-            $this->Exercice_model->add_exercice();
-            redirect('Exercice');
-        }
-        $this->load->view('exercice/add');
+  public function add() {
+    if($this->input->post()) {
+      $this->Exercice_model->add_exercice();
+      redirect('Exercice');
     }
+    $this->load->view('exercice/add');
+  }
 
-    public function edit($id)
-    {
-        if ($this->input->post()) {
-            $this->Exercice_model->edit_exercice($id);
-            redirect('Exercice');
-        }
-        $data['compte'] = $this->Exercice_model->get_exercice_by_id($id);
-        $this->load->view('exercice/edit', $data);
+  public function edit($id) {
+    if($this->input->post()) {
+      $this->Exercice_model->edit_exercice($id);
+      redirect('Exercice');
     }
+    $data['exercice'] = $this->Exercice_model->get_exercice_by_id($id);
+    $this->load->view('exercice/edit', $data);
+  }
 
-    public function delete($id)
-    {
-        $this->Exercice_model->delete_exercice($id);
-        redirect('Exercice');
-    }
+  public function delete($id) {
+    $this->Exercice_model->delete_exercice($id);
+    redirect('Exercice');
+  }
+
 }
